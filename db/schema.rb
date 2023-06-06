21b675d74a18c742a3449c16f8887908d5294b23
Etienne Leguay <etienneleguay@icloud.com>
2023-06-06 16:59:45 -0400

    Creating the seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_06_153419) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_06_202510) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_153419) do
     t.bigint "owner_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["owner_id"], name: "index_activities_on_owner_id"
   end
 
