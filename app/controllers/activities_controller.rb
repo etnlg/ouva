ea3313d222299e23ce245bd2a835c020f80a16c3
Etienne Leguay <etienneleguay@icloud.com>
2023-06-09 10:40:44 -0400

    Fixed the add activity thing

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -4,20 +4,18 @@ class ActivitiesController < ApplicationController
     @start_date = params[:start_date]
     @end_date = params[:end_date]
     @location = params[:location]
+    @day_date = params[:day_date]
     @activities = Activity.near(@location, 100)
   end
 
   def show
     @location = params[:location]
     @activity = Activity.find(params[:id])
-
     number_of_reviews = 3
     @reviews = @activity.random_reviews(number_of_reviews)
-
-
     @start_date = params[:start_date]
     @end_date = params[:end_date]
-
+    @day_date = params[:day_date]
   end
 
 end
